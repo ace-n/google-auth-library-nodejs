3ad74e61f7e2bbe59a536bb1c5f509c8ff42a947
Ace Nassri <anassri@google.com>
2018-01-26 17:10:24 -0800

    Fix TS error

--- src/auth/computeclient.ts
+++ src/auth/computeclient.ts
@@ -36,7 +36,7 @@ export class Compute extends OAuth2Client {
    * See: https://developers.google.com/compute/docs/authentication
    */
   constructor(options?: ComputeOptions) {
-    console.log('MY URL', _GOOGLE_OAUTH2_TOKEN_URL);
+    console.log('MY URL', Compute._GOOGLE_OAUTH2_TOKEN_URL);
     super(options);
     // Start with an expired refresh token, which will automatically be
     // refreshed before the first API call is made.
